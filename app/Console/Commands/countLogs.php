fd1dbc56c3e442ba290553bac383f5320eb4d690
Hubert Wanga MBA <hubert.wanga@gmail.com>
2022-03-12 21:39:42 +0100

    Restrict show logged operations from last 13h and 33min

--- app/Console/Commands/countLogs.php
+++ app/Console/Commands/countLogs.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Log;
 
 class countLogs extends Command
 {
@@ -11,14 +12,14 @@ class countLogs extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'count:logs';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Command counting CRUD logs from last 13h 33min';
 
     /**
      * Create a new command instance.
@@ -37,6 +38,38 @@ class countLogs extends Command
      */
     public function handle()
     {
-        return 0;
+        $countedTime = date("Y-m-d H:i:s", strtotime("-13 hours -33 minutes"));
+
+        $allLogs = Log::where('created_at', '>', $countedTime)->count();
+        $COperations = Log::where('type','C')->where('created_at', '>', $countedTime)->count();
+        $ROperations = Log::where('type','R')->where('created_at', '>', $countedTime)->count();
+        $UOperations = Log::where('type','U')->where('created_at', '>', $countedTime)->count();
+        $DOperations = Log::where('type','D')->where('created_at', '>', $countedTime)->count();
+        $COperationsComment = Log::where('type','C')->where('model','C')->where('created_at', '>', $countedTime)->count();
+        $ROperationsComment = Log::where('type','R')->where('model','C')->where('created_at', '>', $countedTime)->count();
+        $UOperationsComment = Log::where('type','U')->where('model','C')->where('created_at', '>', $countedTime)->count();
+        $DOperationsComment = Log::where('type','D')->where('model','C')->where('created_at', '>', $countedTime)->count();
+        $COperationsPost = Log::where('type','C')->where('model','P')->where('created_at', '>', $countedTime)->count();
+        $ROperationsPost = Log::where('type','R')->where('model','P')->where('created_at', '>', $countedTime)->count();
+        $UOperationsPost = Log::where('type','U')->where('model','P')->where('created_at', '>', $countedTime)->count();
+        $DOperationsPost = Log::where('type','D')->where('model','P')->where('created_at', '>', $countedTime)->count();
+
+        $this->info("Operations counted from: $countedTime");
+        $this->info("All CRUD Operations: $allLogs");
+        $this->info("--------------------------------");
+        $this->info("Create Operations: $COperations");
+        $this->info("Read Operations: $ROperations");
+        $this->info("Update Operations: $UOperations");
+        $this->info("Delete Operations: $DOperations");
+        $this->info("--------------------------------");
+        $this->info("Create Operations on Comments: $COperationsComment");
+        $this->info("Read Operations on Comments: $ROperationsComment");
+        $this->info("Update Operations on Comments: $UOperationsComment");
+        $this->info("Delete Operations on Comments: $DOperationsComment");
+        $this->info("--------------------------------");
+        $this->info("Create Operations on Posts: $COperationsPost");
+        $this->info("Read Operations on Posts: $ROperationsPost");
+        $this->info("Update Operations on Posts: $UOperationsPost");
+        $this->info("Delete Operations on Posts: $DOperationsPost");
     }
 }
